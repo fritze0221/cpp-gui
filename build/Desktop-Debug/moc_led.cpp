c5b626299a9330407edf32427c9eb9b1271b866f
fritze0221 <urs@industry-soft.de>
2025-01-14 17:25:52 +0100

    major update

--- build/Desktop-Debug/moc_led.cpp
+++ build/Desktop-Debug/moc_led.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../led.h"
+#include "../../ledcontrol/led.h"
 #include <QtGui/qtextcursor.h>
 #include <QtCore/qmetatype.h>
 
--- build/Desktop-Debug/moc_modbutton.cpp
+++ build/Desktop-Debug/moc_modbutton.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../traincontrol/modbutton.h"
+#include "../../modbutton.h"
 #include <QtCore/qmetatype.h>
 
 #include <QtCore/qtmochelpers.h>
